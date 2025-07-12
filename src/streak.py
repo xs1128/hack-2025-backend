799ee35bf8863f61ad58cfa008e4341b64025bcc
Unana25 <yuchiao9825@gmail.com>
2025-07-13 02:09:36 +0800

    feat: implement streak tracking and daily check endpoints

--- src/streak.py
+++ src/streak.py
@@ -1,8 +1,25 @@
 from fastapi import APIRouter
+from custom_types import User
+from shared_data import users
+from datetime import datetime
 
 router = APIRouter()
+today = datetime.now().date()
 
+@router.post("/internal/streak")
+def streak():
+    for i in range(len(users)): 
+        if users[i]["store"]["freeze"] >= 1:
+            users[i]['store']['freeze'] -= 1
+        elif users[i]["quiz"]["last_played"] == today:
+            users[i]["quiz"]["current_streak"] += 1
+        else:
+            users[i]["quiz"]["current_streak"] = 0
+    return
 
-@router.get("/sample")
-def sample_endpoint():
-    pass
+@router.post("/did_today/{id}")
+def did_or_not(id: int):
+    if users[id]["quiz"]["last_played"] == today:
+        return 1
+    else:
+        return 0
