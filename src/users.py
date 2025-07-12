c56ce31de11c254966cdeb5c5a1a9801705ba4a8
Xinsheng Ooi <ooixinsheng@gmail.com>
2025-07-13 00:01:36 +0800

    Fix class design, regex for email

--- src/users.py
+++ src/users.py
@@ -14,7 +14,7 @@ def login(email: str):
     if not email:
         raise HTTPException(
             status_code=400, detail="Invalid name and email format")
-    elif re.match(r'^[a-zA-Z0–9._%+-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,}$', email) is None:
+    elif re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email) is None:
         raise HTTPException(status_code=422, detail="Invalid email")
 
     for user in users:
@@ -33,12 +33,12 @@ def login(email: str):
             "last_played": datetime.now(),
             "current_streak": 0,
             "solved_quiz": 0,
-            "ranking": 1,
         },
         "store": {
             "coin": 0,
             "freeze": 0,
         },
+        "ranking": 1,
     }
 
     users.append(new_user)
