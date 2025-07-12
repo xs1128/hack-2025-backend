d108783a0b9795c062b84ca96cfcdd18a5a9bce9
Xinsheng Ooi <ooixinsheng@gmail.com>
2025-07-12 21:36:37 +0800

    Add store interface for routing

---
+++ src/store.py
@@ -0,0 +1,30 @@
+from custom_types import User
+from shared_data import users
+from fastapi import APIRouter, HTTPException
+from typing import TypedDict
+
+router = APIRouter()
+
+# Price mapping
+price_mapping = {
+    "freeze": 15,
+}
+
+
+# Store site
+@router.get("/store/{id}")
+def get_store(id: int):
+    # Encapsulation later
+    return users[0]["store"]
+
+
+@router.post("/store/{id}")
+def post_store(id: int):
+    # Check if enough money
+    if users[id]["store"]["coin"] < price_mapping["freeze"]:
+        return HTTPException(status_code=400, detail="Not enough coins!")
+
+    users[id]["store"]["coin"] -= price_mapping["freeze"]
+    users[id]["store"]["freeze"] += 1
+
+    return {"user": users[id]}
