2ae4c741b14517f9e75663b0e234032004e96f75
burnedinthesky <andrew@kuo.is>
2025-07-13 11:20:49 +0800

    feat: add cors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from users import router as users_router
 from leagues import router as leagues_router
@@ -8,6 +9,15 @@ from streak import router as streak_router
 from store import router as store_router
 
 app = FastAPI()
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for development purposes
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(leagues_router)
